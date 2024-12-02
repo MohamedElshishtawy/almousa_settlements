89fad182c4d268a4ce4544c5698cff670e3ee07d
MohamedElshishtawy <mohamed.elshishtawy11@gmail.com>
2024-12-02 11:36:49 +0200

    debug

--- resources/views/reports/import-writing-print.blade.php
+++ resources/views/reports/import-writing-print.blade.php
@@ -15,9 +15,9 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Amiri:ital,wght@0,400;0,700;1,400;1,700&family=Cairo:wght@200..1000&family=Indie+Flower&family=Lalezar&family=Matemasie&family=Permanent+Marker&family=Playwrite+FR+Moderne:wght@100..400&family=Readex+Pro:wght@160..700&family=Sen:wght@400..800&family=Tajawal:wght@200;300;400;500;700;800;900&family=Titan+One&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{asset('css/print-page-media.css?7')}}" media="print">
+    <link rel="stylesheet" href="{{asset('css/print-page-media.css?8')}}" media="print">
     <title>محضر توريد كتابى</title>
-    <link rel="stylesheet" href="{{asset('css/print-page.css?7')}}">
+    <link rel="stylesheet" href="{{asset('css/print-page.css?8')}}">
 </head>
 <body dir="rtl" class="import-writing " >
 <main>
