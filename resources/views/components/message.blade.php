3e228b59eeddeb12fb6be9c386a725bc118761ee
MohamedElshishtawy <mohamed.elshishtawy11@gmail.com>
2025-02-10 14:58:11 +0200

    permissions table

--- resources/views/components/message.blade.php
+++ resources/views/components/message.blade.php
@@ -12,9 +12,35 @@
 @endif
 
 @if(session('error'))
-    <div class="alert alert-danger text-right my-4">{{session('error')}}</div>
+    <div class="alert alert-danger float-message" role="alert">
+        <div>
+            {{session('error')}}
+        </div>
+        <button type="button" class="close-message">
+            <i class="fa fa-xmark"></i>
+        </button>
+    </div>
 @endif
 
-@if(session('message'))
-    <div class="alert alert-info text-right my-4">{{session('message')}}</div>
+@if(session('warning'))
+    <div class="alert alert-warning float-message" role="alert">
+        <div>
+            {{session('warning')}}
+        </div>
+        <button type="button" class="close-message">
+            <i class="fa fa-xmark"></i>
+        </button>
+    </div>
+@endif
+
+@if(session('info'))
+    <div class="alert alert-info float-message" role="alert">
+        <div>
+            {{session('info')}}
+        </div>
+        <button type="button" class="close-message">
+            <i class="fa fa-xmark"></i>
+        </button>
+    </div>
+
 @endif
