5dd5106508847dc6c0260c4f0e3507b6ae882586
MohamedElshishtawy <mohamed.elshishtawy11@gmail.com>
2025-02-13 02:10:44 +0200

    making history for delegates

--- resources/views/analytics/print-layout.blade.php
+++ resources/views/analytics/print-layout.blade.php
@@ -32,9 +32,11 @@
 <x-nav :active="$active ?? null"></x-nav>
 @yield('content')
 
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
+<script src="{{asset('js/toggle_table.js?3')}}"></script>
 
 </body>
 </html>
