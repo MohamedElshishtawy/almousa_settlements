b6c8e122407328151c0085069994ff91e4f7a5ac
MohamedElshishtawy <mohamed.elshishtawy11@gmail.com>
2025-02-23 23:12:36 +0200

    bug the routs

--- routes/web.php
+++ routes/web.php
@@ -30,17 +30,6 @@ use Livewire\Livewire;
 |
 */
 
-Route::get('/me', function () {
-    $ramadanMainQiada = \App\Product\ProductLivingMission::where('mission_id', 2)->where('living_id',
-        1)->get(); // have prices
-    foreach ($ramadanMainQiada as $product) {
-        $product = \App\Product\ProductLivingMission::where('product_id', $product->product_id)->where('mission_id',
-            '4')->where('living_id', 1)->first();
-        if ($product) {
-            $product->update(['price' => $product->price]);
-        }
-    }
-});
 
 Livewire::setUpdateRoute(function ($handle) {
     return Route::post(env('LARAVEL_URL').'/livewire/update', $handle);
@@ -71,21 +60,22 @@ Route::middleware('auth')->group(function () {
     Route::get('permission-management', [AdminController::class, 'permissionManagement'])
         ->name('admin.permission-management')->middleware('role:admin');
 
-    Route::prefix('users')->group(function () {
+    Route::prefix('users')->middleware('permission:users_management')->group(function () {
         Route::get('/', [UserController::class, 'index'])->name('admin.users');
         Route::get('/create', [UserController::class, 'create'])->name('admin.users.create');
         Route::get('/{user}', [UserController::class, 'edit'])->name('admin.users.edit');
         Route::delete('/{user}', [UserController::class, 'delete'])->name('admin.users.delete');
-    })->middleware('permission:users_management');
+    });
 
-    Route::prefix('offices')->group(function () {
+    Route::prefix('offices')->middleware('can:offices_management')->group(function () {
         Route::get('/', [OfficeController::class, 'offices'])->name('admin.offices');
         Route::get('/create', [OfficeController::class, 'CreateOffice'])->name('admin.office.create');
         Route::get('/{id}', [OfficeController::class, 'EditOffice'])->name('admin.office.edit');
         Route::delete('/{id}', [OfficeController::class, 'DeleteOffice'])->name('admin.office.delete');
-    })->middleware('can:offices_management');
+    });
 
-    Route::prefix('products')->group(function () {
+    Route::prefix('products')->middleware('permission:manage_all_products|manage_mission_products|break_fast_products_manage')->group(function (
+    ) {
         Route::get('/', [AdminController::class, 'products'])->name('admin.products');
         Route::get('/all',
             [AdminController::class, 'all'])->name('admin.products.all')->middleware('permission:manage_all_products');
@@ -95,18 +85,20 @@ Route::middleware('auth')->group(function () {
         Route::get('/breakfast-products', [
             BreakFastProductController::class, 'index'
         ])->name('admin.breakfast-products')->middleware('permission:break_fast_products_manage');
-    })->middleware('permission:manage_all_products|manage_mission_products|break_fast_products_manage');
+    });
 
-    Route::prefix('employment')->group(function () {
+    Route::prefix('employment')->middleware('permission:employment_create|employment_edit|employment_delete')->group(function (
+    ) {
         Route::get('/', [EmploymentController::class, 'employment'])->name('admin.employment');
-    })->middleware('permission:employment_create|employment_edit|employment_delete');
+    });
 
-    Route::prefix('evaluate')->group(function () {
+    Route::prefix('evaluate')->middleware('permission:evaluation_create|evaluation_edit|evaluation_delete')->group(function (
+    ) {
         Route::get('/', [UserEvaluateController::class, 'index'])->name('admin.evaluate.index');
         Route::get('/manage', [UserEvaluateController::class, 'manage'])->name('admin.evaluate.manager');
         Route::get('/manage/{user}', [UserEvaluateController::class, 'manage'])->name('admin.evaluate.manager.');
         Route::get('/{user}', [UserEvaluateController::class, 'evaluate'])->name('admin.evaluate.user');
-    })->middleware('permission:evaluation_create|evaluation_edit|evaluation_delete');
+    });
 
     Route::prefix('analytics')->group(function () {
         Route::get('/imports/{showPrices?}', [
@@ -132,17 +124,18 @@ Route::middleware('auth')->group(function () {
     Route::get('/dates',
         [HijriDateController::class, 'index'])->name('admin.dates')->middleware('permission:manage_dates');
 
-    Route::prefix('tasks')->group(function () {
+    Route::prefix('tasks')->middleware('permission:tasks_create|tasks_edit|tasks_delete')->group(function () {
         Route::get('/', [TaskController::class, 'index'])->name('admin.tasks');
         Route::get('/{officeId}', [TaskController::class, 'officeTasks'])->name('admin.tasks.office');
         Route::post('/{officeId}/store',
             [TaskController::class, 'storeTask'])->name('admin.tasks.store')->middleware('permission:tasks_create');
         Route::get('/{officeId?}/show', [TaskController::class, 'show'])->name('admin.tasks.managers');
 
-    })->middleware('permission:tasks_create|tasks_edit|tasks_delete');
+    });
 
 
-    Route::prefix('/delegates')->group(function () {
+    Route::prefix('/delegates')->middleware('permission:delegate_create|delegate_edit|delegate_delete')->group(function (
+    ) {
         Route::get('/', [DelegateController::class, 'index'])->name('admin.delegates');
         Route::get('/create', [
             DelegateController::class, 'create'
@@ -152,9 +145,10 @@ Route::middleware('auth')->group(function () {
         Route::delete('/{delegate}', [
             DelegateController::class, 'delete'
         ])->name('admin.delegates.delete')->middleware('permission:delegate_delete');
-    })->middleware('permission:delegate_create|delegate_edit|delegate_delete');
+    });
 
-    Route::prefix('breakfast')->group(function () {
+    Route::prefix('breakfast')->middleware('permission:break_fast_create|break_fast_edit|break_fast_delete|break_fast_print')->group(function (
+    ) {
         Route::get('/', [BreakFastReportController::class, 'index'])
             ->name('breakfast.index');
         Route::get('/create', [BreakFastReportController::class, 'create'])
@@ -169,12 +163,13 @@ Route::middleware('auth')->group(function () {
             ->name('breakfast.destroy')->middleware('permission:break_fast_delete');
         Route::get('/{breakfast}/print', [BreakFastReportController::class, 'print'])
             ->name('breakfast.print')->middleware('permission:break_fast_print');
-    })->middleware('permission:break_fast_create|break_fast_edit|break_fast_delete|break_fast_print');
+    });
 
     Route::get('/breakfast/{breakFastReport}/print', [BreakFastReportController::class, 'print'])
         ->name('breakfast.print')->middleware('permission:break_fast_print');
 
-    Route::prefix('/reports')->group(function () {
+    Route::prefix('/reports')->middleware('permission:import_create|import_edit|import_delete|import_print|surplus_create|surplus_edit|surplus_delete|surplus_print')->group(function (
+    ) {
         Route::get('/', [ReportController::class, 'reports'])->name('managers.reports');
         Route::prefix('/import')->group(function () {
             Route::get('/{officeMission}/{date}', [
@@ -197,9 +192,10 @@ Route::middleware('auth')->group(function () {
                     ReportController::class, 'surplus'
                 ])->name('managers.reports.surplus')->middleware('permission:surplus_create|surplus_edit|surplus_delete');
         });
-    })->middleware('permission:import_create|import_edit|import_delete|import_print|surplus_create|surplus_edit|surplus_delete|surplus_print');
+    });
 
-    Route::prefix('/obligations')->group(function () {
+    Route::prefix('/obligations')->middleware('permission:obligations_create|obligations_edit|obligations_delete|obligations_print')->group(function (
+    ) {
         Route::get('/', [ObligationsController::class, 'index'])->name('obligations');
         Route::get('/create', [
             ObligationsController::class, 'create'
@@ -213,14 +209,15 @@ Route::middleware('auth')->group(function () {
         Route::delete('/{obligation}', [
             ObligationsController::class, 'delete'
         ])->name('obligations.destroy')->middleware('permission:obligations_delete');
-    })->middleware('permission:obligations_create|obligations_edit|obligations_delete|obligations_print');
+    });
 
-    Route::prefix('employment-form/{import}')->group(function () {
+    Route::prefix('employment-form/{import}')->middleware('permission:employment_create|employment_edit|employment_delete')->group(function (
+    ) {
         Route::get('/', [EmploymentController::class, 'employmentForm'])
             ->name('managers.employment');
         Route::get('/print', [EmploymentController::class, 'employmentFormPrint'])
             ->name('managers.employment.print')->middleware('permission:employment_print');
-    })->middleware('permission:employment_create|employment_edit|employment_delete');
+    });
 
 
     Route::get('/papers/delegate-rejects',
@@ -228,13 +225,14 @@ Route::middleware('auth')->group(function () {
             \App\Models\Delegate::class, 'rejects'
         ])->name('papers.doesNotWant')->middleware('permission:delegate_rejects_management');
 
-    Route::prefix('delegate-abcence')->group(function () {
+    Route::prefix('delegate-abcence')->middleware('permission:delegate_absence_create|delegate_absence_edit|delegate_absence_delete|delegate_absence_print')->group(function (
+    ) {
         Route::get('/', [DelegateAbsenceController::class, 'index'])->name('delegate-absence');
         Route::get('/{delegateAbcence}',
             [
                 DelegateAbsenceController::class, 'printPage'
             ])->name('delegate-absence.print')->middleware('permission:delegate_absence_print');
-    })->middleware('permission:delegate_absence_create|delegate_absence_edit|delegate_absence_delete|delegate_absence_print');
+    });
 
 
     Route::middleware('permission:dry_food_create|dry_food_edit|dry_food_delete|dry_food_print')->group(function () {
