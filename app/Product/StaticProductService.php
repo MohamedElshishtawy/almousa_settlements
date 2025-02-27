b512a95d7e12b4ca422b7e77571a0cf594ce5399
MohamedElshishtawy <mohamed.elshishtawy11@gmail.com>
2025-02-27 08:06:43 +0200

    surplus to be using bcmath

--- app/Product/StaticProductService.php
+++ app/Product/StaticProductService.php
@@ -119,7 +119,7 @@ class StaticProductService
         $total = max($thisDayImported - $totalSurplus, 0);
 
         return [
-            'amountForMeal' => round($amountForMeal, 10),
+            'amountForMeal' => number_format($amountForMeal, 10),
             'totalAmountForMeal' => round($importData['totalAmountForMeal'], 4),
             'thisDayImported' => round($thisDayImported, 4),
             'surplusProductError' => round($surplusProductErrorAmount, 4),
